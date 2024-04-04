d5533f4946700cdaac214d6c092b26ccf52d22e3
Griffin <griffin.muuo@student.moringaschool.com>
2024-04-04 09:50:12 +0300

    PC

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
 
-// Write your code here
+let random = 6;
+const num3 = 80;
+const num4 = 20;
+const mod = num3 / num4;
+
+
+const max = Math.max(12,13,4,20);
